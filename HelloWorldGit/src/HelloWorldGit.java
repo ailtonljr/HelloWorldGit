85143a8969f09773f92f38c4645b16b71bd47291
Ailton Luiz Dias Siqueira Junior <ailtonljr@gmail.com>
2015-05-29 10:51:33 -0300

    Edição no GitHub

--- HelloWorldGit/src/HelloWorldGit.java
+++ HelloWorldGit/src/HelloWorldGit.java
@@ -6,8 +6,10 @@ public class HelloWorldGit {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Ol· Mundo do Git!!!");
+		System.out.println("Ol√° Mundo do Git!!!");
 		System.out.println("Estou no servidor.");
+		
+		System.out.println("Editado no servidor");
 	}
 
 }
